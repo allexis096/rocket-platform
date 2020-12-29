ea9164b28e39c8f4943005dd76c555dd5e75d422
allexis <allexissoares@gmail.com>
2020-12-28 21:09:25 -0300

    style: add width and fill svg with right color

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react';
+import { InputHTMLAttributes, useState } from 'react';
 import { IconType } from 'react-icons';
 import { BsEyeFill, BsEyeSlashFill } from 'react-icons/bs';
 
-interface InputProps {
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   placeholder: string;
   icon: IconType;
 }
@@ -16,13 +16,13 @@ export default function Input({ placeholder, icon: Icon }: InputProps) {
       <Icon
         className="absolute top-4 left-4"
         color="#121214"
-        fill={`${focus === true ? '#8464e5' : '#202024'}`}
+        fill={`${focus === true ? '#8464e5' : '#29292e'}`}
         size={14}
       />
       {placeholder === 'Senha' ? (
         <>
           <input
-            className="bg-primary h-12 pr-10 pl-11 w-80 rounded-md mb-4 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-75"
+            className="bg-primary h-12 pr-10 pl-11 w-352px rounded-md mb-4 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-75"
             type={showPassword === false ? 'password' : 'text'}
             placeholder={placeholder}
             onFocus={() => setFocus(!focus)}
@@ -48,7 +48,7 @@ export default function Input({ placeholder, icon: Icon }: InputProps) {
         </>
       ) : (
         <input
-          className="bg-primary h-12 pr-6 pl-11 w-80 rounded-md mb-4 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-75"
+          className="bg-primary h-12 pr-6 pl-11 w-352px rounded-md mb-4 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-75"
           type="text"
           placeholder={placeholder}
           onFocus={() => setFocus(!focus)}
