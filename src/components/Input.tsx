e309c522f4e8092a5ea89f4ac6be66c45cd5d0b9
allexis <allexissoares@gmail.com>
2020-12-30 21:40:48 -0300

    feat: add Input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,62 @@
+import { InputHTMLAttributes, useState } from 'react';
+
+import { Container } from '../styles/components/Input';
+import { IconType } from 'react-icons';
+import { IoMdEye, IoMdEyeOff } from 'react-icons/io';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  placeholder: string;
+  password?: boolean;
+  icon: IconType;
+}
+
+export default function Input({ placeholder, icon: Icon, password }: InputProps) {
+  const [showPassword, setShowPassword] = useState(false);
+  const [focus, setFocus] = useState(false);
+
+  if (password) {
+    return (
+      <Container>
+        <Icon
+          size={15}
+          fill={`${focus === true ? '#8464e5' : '#202024'}`}
+          color="#121214"
+          style={{ margin: '2px 3px' }}
+        />
+        <input
+          style={{ paddingRight: '2.5em' }}
+          className="password"
+          type={showPassword === false ? 'password' : 'text'}
+          placeholder={placeholder}
+          onFocus={() => setFocus((prev) => !prev)}
+          onBlur={() => setFocus((prev) => !prev)}
+        />
+        {showPassword === false ? (
+          <IoMdEye
+            onClick={() => setShowPassword((prev) => !prev)}
+            className="eye-password"
+            size={18}
+          />
+        ) : (
+          <IoMdEyeOff
+            onClick={() => setShowPassword((prev) => !prev)}
+            className="eye-password"
+            size={18}
+          />
+        )}
+      </Container>
+    );
+  }
+
+  return (
+    <Container>
+      <Icon size={20} fill={`${focus === true ? '#8464e5' : '#202024'}`} color="#121214" />
+      <input
+        type="text"
+        placeholder={placeholder}
+        onFocus={() => setFocus((prev) => !prev)}
+        onBlur={() => setFocus((prev) => !prev)}
+      />
+    </Container>
+  );
+}
