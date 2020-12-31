cc8a7b1bc5577823e27a1dde824a938e00aa586a
allexis <allexissoares@gmail.com>
2020-12-31 12:50:43 -0300

    Revert "chore: add jest and testing library dependencies"
    This reverts commit d0359f71802056bf34af884faea060ad14f9e0fa.

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-  bail: 1,
-  clearMocks: true,
-  collectCoverage: true,
-  collectCoverageFrom: ['src/components/*.tsx', 'src/pages/*.tsx', "!src/pages/_app.tsx", "!src/pages/_document.tsx"],
-  coverageReporters: ['lcov', 'text'],
-};
