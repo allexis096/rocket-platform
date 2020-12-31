a9a0f40394bdb8e971809c760d6164bb8a54af00
allexis <allexissoares@gmail.com>
2020-12-31 10:04:32 -0300

    feat: add index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,61 @@
+import Link from 'next/link';
+import { FiMail } from 'react-icons/fi';
+import { FaLock, FaGithub } from 'react-icons/fa';
+
+import {
+  Container,
+  Header,
+  Main,
+  Content,
+  SignInButton,
+  CreateAccount,
+  Divider,
+  GithubAction,
+} from '../styles/pages/index';
+import Logo from '../../public/logo.svg';
+import Input from '../components/Input';
+
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <Container>
+      <Header>
+        <Logo />
+        <h1>
+          Faça seu login <br />
+          na plataforma
+        </h1>
+      </Header>
+      <Main>
+        <Content>
+          <Input icon={FiMail} placeholder="E-mail" />
+          <Input icon={FaLock} placeholder="Senha" password />
+          <Link href="/">
+            <a>Esqueci minha senha</a>
+          </Link>
+
+          <SignInButton>Entrar</SignInButton>
+
+          <CreateAccount>
+            Não tem uma conta?{' '}
+            <Link href="/">
+              <a>Registre-se</a>
+            </Link>
+          </CreateAccount>
+
+          <Divider>
+            <div />
+            <div />
+          </Divider>
+
+          <GithubAction>
+            <span>Ou entre com</span>
+            <button type="button">
+              <FaGithub size={20} />
+              Github
+            </button>
+          </GithubAction>
+        </Content>
+      </Main>
+    </Container>
+  );
 }
