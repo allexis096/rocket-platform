235187139955970984f32f2cb17fb32237f3aa5e
allexis <allexissoares@gmail.com>
2021-01-02 19:01:25 -0300

    feat(test): add tests to Input

---
+++ src/__tests__/components/Input.test.tsx
@@ -0,0 +1,69 @@
+import { fireEvent, render, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Input from '../../components/Input';
+import { IconBase } from 'react-icons';
+
+describe('Input component', () => {
+  it('should be able to render Input Password with focus and blur', async () => {
+    const setFocus = jest.fn();
+
+    const { getByPlaceholderText } = render(
+      <Input
+        placeholder="Senha"
+        password
+        icon={IconBase}
+        onFocus={setFocus()}
+        onBlur={setFocus()}
+      />
+    );
+
+    const passwordInput = getByPlaceholderText('Senha');
+
+    passwordInput.focus();
+    passwordInput.blur();
+
+    await waitFor(() => {
+      expect(setFocus).toHaveBeenCalledTimes(2);
+    });
+  });
+
+  it('should be able to switch between password and text on Input Password', async () => {
+    const { getByPlaceholderText, getByTestId } = render(
+      <Input placeholder="Senha" password icon={IconBase} />
+    );
+
+    const passwordInput = getByPlaceholderText('Senha');
+    const test = getByTestId('eye-password');
+
+    fireEvent.click(test);
+
+    await waitFor(() => {
+      expect(passwordInput).toHaveProperty('type', 'text');
+    });
+
+    const test2 = getByTestId('eye-passwordText');
+
+    fireEvent.click(test2);
+
+    await waitFor(() => {
+      expect(passwordInput).toHaveProperty('type', 'password');
+    });
+  });
+
+  it('should be able to render a common Input with focus and blur', async () => {
+    const setFocus = jest.fn();
+
+    const { getByPlaceholderText } = render(
+      <Input placeholder="E-mail" icon={IconBase} onFocus={setFocus()} onBlur={setFocus()} />
+    );
+
+    const passwordInput = getByPlaceholderText('E-mail');
+
+    passwordInput.focus();
+    passwordInput.blur();
+
+    await waitFor(() => {
+      expect(setFocus).toHaveBeenCalledTimes(2);
+    });
+  });
+});
