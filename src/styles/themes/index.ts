81839751b9a1660cc1d6bc521a805129225af3c9
allexis <allexissoares@gmail.com>
2021-01-03 20:49:19 -0300

    feat: add Light and Dark theme

---
+++ src/styles/themes/index.ts
@@ -0,0 +1,34 @@
+const dark = {
+  colors: {
+    primary: '#8257e6',
+    secondary: '#202024',
+    tertiary: '#e1e1e6',
+
+    primaryHover: '#9f80e8',
+    text: '#fff',
+    span: '#ccc',
+    divider: '#28272f',
+    button: '#29292e',
+
+    background: '#121214',
+  },
+};
+
+const light = {
+  colors: {
+    primary: '#8257e6',
+    secondary: '#aaa',
+    tertiary: '#121214',
+
+    primaryHover: '#9f80e8',
+    text: '#fff',
+    span: '#121214',
+    divider: '#28272f',
+    button: '#86888a',
+
+    background: '#eee',
+  },
+};
+
+export const darkTheme = { ...dark };
+export const lightTheme = { ...light };
