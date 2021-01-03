be53f9e6d52a373ae2ba43f267901cbed74fde83
allexis <allexissoares@gmail.com>
2021-01-03 20:48:32 -0300

    refactor: using Providers.tsx instead ThemeProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,21 @@
+import App from 'next/app';
 import Head from 'next/head';
 import GlobalStyles from '../styles/GlobalStyles';
+import Providers from '../styles/themes/Providers';
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <title>Login | Rocketseat</title>
-      </Head>
-      <Component {...pageProps} />
-      <GlobalStyles />
-    </>
-  );
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <Providers>
+        <Head>
+          <title>Login | Rocketseat</title>
+        </Head>
+        <Component {...pageProps} />
+        <GlobalStyles />
+      </Providers>
+    );
+  }
 }
 
 export default MyApp;
