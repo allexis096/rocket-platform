c308c3c7dbc8f9ac56c604fa26ea629eeac4758d
allexis <allexissoares@gmail.com>
2021-01-09 13:38:11 -0300

    feat: add Header and CardCourses components to dashboard page

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,32 @@
 import Header from '../components/Header';
+import CardCourses from '../components/CardCourses';
+import Discover from '../../public/discover.svg';
+import GoStack from '../../public/gostack.svg';
+import Expert from '../../public/expert.svg';
+
+import { Courses } from '../styles/pages/dashboard';
 
 export default function Dashboard() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Courses>
+        <CardCourses
+          name="discover"
+          image={<Discover />}
+          text="Comunidade e conteúdo gratuito que te levarão para o próximo nível em programação"
+        />
+        <CardCourses
+          name="gostack"
+          image={<GoStack />}
+          text="Treinamento imersivo nas tecnologias mais modernas de desenvolvimento web e mobile"
+        />
+        <CardCourses
+          name="expert"
+          image={<Expert />}
+          text="Aulas práticas e direto ao ponto com profissionais que respiram tecnologia diariamente"
+        />
+      </Courses>
+    </>
+  );
 }
